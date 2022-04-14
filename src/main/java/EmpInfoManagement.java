d264384fabdecdad49126ab47da9184580b7164f
JaeHee <leejeahue@gmail.com>
2022-04-14 14:32:31 +0900

    폴더구조 push

---
+++ src/main/java/EmpInfoManagement.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+class Employee{
+    int employeeNum;
+    String name;
+    String cl;
+    int phoneNum;
+    String birthday;
+    String certi;
+}
+
+public class EmpInfoManagement {
+    public static ArrayList<Employee> Myemp=new ArrayList<>();
+
+    public static void main(String[]args){
+        //input.txt 구현
+
+    }
+
+    void RUN(){};//초기 imput.txt 파일을 받아 한줄 씩 읽어 각 함수를 실행할 수 있도록 구현하여 output txt 파일 생성
+    void ADD(){};//데이터 한줄씩 add
+    void DEL(){};//SCH 를 활용하여 삭제
+    void SCH(){};
+    void MOD(){};//SCH 를 황용하여 MOD
+
+
+}
---
+++ src/test/java/EmplnfoManagementTest.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EmplnfoManagementTest {
+
+
+    @BeforeEach
+    void SetUp() {
+        EmpInfoManagement abc=new EmpInfoManagement();
+
+    }
+
+    @Test
+    void ADD_TEST() {
+
+
+    }
+
+    @Test
+    void DEL_TEST() {
+
+
+    }
+
+    @Test
+    void SCH_TEST() {
+
+
+    }
+
+    @Test
+    void MOD_TEST() {
+
+
+    }
+}
