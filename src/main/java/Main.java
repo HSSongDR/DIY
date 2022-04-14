eba216abef4308845d8b07e3afed1f5841d37cc1
pc <pc@DESKTOP-2KC329R>
2022-04-14 14:21:14 +0900

    최초커밋 폴더구조 및 최초 java파일 추가

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Main {
+	
+	public static ArrayList<EmployeeInfo> empList = new ArrayList<>();
+	
+	public static void main(String[] args) {
+		
+		//input 구현
+	}
+	
+	void RUN() {};
+
+}
