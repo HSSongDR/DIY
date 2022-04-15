fb06cbd0402d6ef37b192aeb21e88f9a5197f7d5
곽민승 <103616574+kwakminseung@users.noreply.github.com>
2022-04-15 13:45:46 +0900

    development_ms
    조회 조건에 따라 Key 값 지정하여 SearchHM<key, arrayList<String>> 생성.

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,8 +1,75 @@
 
+import java.util.ArrayList;
+
 public class DB {
-	
-	public void ADD() {};
-	public void DEL() {};
-	public void SCH() {};
-	public void MOD() {};
+
+    private static void createSearchHM(EmployeeInfo newEmployeeInfo) {
+        String sEmployeeNum = newEmployeeInfo.getEmployeeNum();
+        String name = newEmployeeInfo.getName();
+        checkSearchHM(name, sEmployeeNum);
+
+        String fname = "FNAME"+name.split(" ")[0];
+        checkSearchHM(fname, sEmployeeNum);
+
+        String lname = "LNAME"+name.split(" ")[1];
+        checkSearchHM(lname, sEmployeeNum);
+
+        String cl = newEmployeeInfo.getCl();
+        checkSearchHM(cl, sEmployeeNum);
+
+        String phoneNum = newEmployeeInfo.getPhoneNum();
+        checkSearchHM(phoneNum, sEmployeeNum);
+
+        String fphone = "FPHONE"+phoneNum.split("-")[1];
+        checkSearchHM(fphone, sEmployeeNum);
+
+        String lphone = "LPHONE"+phoneNum.split("-")[2];
+        checkSearchHM(lphone, sEmployeeNum);
+
+        String birthday = "BIRTH" + newEmployeeInfo.getBirthday();
+        checkSearchHM(birthday, sEmployeeNum);
+
+        String ybirthday = "Y" + birthday.substring(0, 4);
+        checkSearchHM(ybirthday, sEmployeeNum);
+
+        String mbirthday = "M" + birthday.substring(4, 6);
+        checkSearchHM(mbirthday, sEmployeeNum);
+
+        String dbirthday = "D" + birthday.substring(6, 8);
+        checkSearchHM(dbirthday, sEmployeeNum);
+
+        String certi = newEmployeeInfo.getCerti();
+        checkSearchHM(certi, sEmployeeNum);
+
+    }
+
+    private static void checkSearchHM(String searchkey, String newEmployeeNum) {
+        if (Main.searchHM.get(searchkey) == null) {
+            // serachHM신규 생성.
+            ArrayList<String> list = new ArrayList<String>();
+            list.add(newEmployeeNum);
+            Main.searchHM.put(searchkey, list);
+        } else {
+            Main.searchHM.get(searchkey).add(newEmployeeNum);
+        }
+    }
+
+    public static void ADD(String[] tempsplit) {
+        if (tempsplit.length < 10) System.out.println("ADD_FAIL :: Insert Null Data");
+
+        EmployeeInfo newEmployeeInfo =
+                new EmployeeInfo(tempsplit[4], tempsplit[5], tempsplit[6], tempsplit[7], tempsplit[8], tempsplit[9]);
+        Main.employeeHM.put(tempsplit[4], newEmployeeInfo);
+
+        createSearchHM(newEmployeeInfo);
+    }
+
+    public void DEL() {
+    }
+
+    public void SCH() {
+    }
+
+    public void MOD() {
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,49 @@
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Main {
-	
-	public static ArrayList<EmployeeInfo> empList = new ArrayList<>();
-	
-	public static void main(String[] args) {
-		
-		//input 구현
+
+	protected static HashMap<String, EmployeeInfo> employeeHM;
+	protected static HashMap<String, ArrayList<String>> searchHM;
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new FileReader("./DIY/src/input.txt"));
+
+		int n = Integer.parseInt(br.readLine());
+		employeeHM = new HashMap<>();
+		searchHM = new HashMap<>();
+
+		for (int i = 0; i < n; i++) {
+			String temp = br.readLine();
+			String[] tempsplit = temp.split(",");
+
+			if (tempsplit[0].equals("ADD")) {
+				DB.ADD(tempsplit);
+			} else if (tempsplit[0].equals("SCH")) {
+
+			} else if (tempsplit[0].equals("DEL")) {
+
+			} else if (tempsplit[0].equals("MOD")) {
+
+			}
+		}
+/*
+		System.out.println(employeeHM.size());
+		System.out.println("employeeHM.get(\"12000001\").name :" + employeeHM.get("12000001").getName());
+		System.out.println("employeeHM.get(\"12000001\").birthday :" + employeeHM.get("12000001").getBirthday());
+		System.out.println("employeeHM.get(\"12000001\").certi :" + employeeHM.get("12000001").getCerti());
+		if (searchHM.get("FPHONE1234").size() > 0) {
+			for (String getNumber : searchHM.get("FPHONE1234")) {
+				System.out.println("searchHM.get(\"1234\") :" + getNumber);
+			}
+		}
+
+ */
 	}
-	
-	void RUN() {};
 
 }
