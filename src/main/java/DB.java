e70fc6f79ca3d1d1bdc0f7d9ded03be46c2f1052
곽민승 <103616574+kwakminseung@users.noreply.github.com>
2022-04-18 08:29:43 +0900

    development_ms
    ADD/MOD/DEL/SCH 구현 완료버젼입니다.
    사번 조회시 PriorityQueue를 이용해서 아래 항목의 추가구현을 하고 결과 출력부분 수정하면 일단 기능 수행은 완료될것 같습니다.
    1. 사번순 오름차순 정렬
    2. 동일사번의 중복 출력

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,76 +1,199 @@
 
+import java.io.File;
+import java.io.FileWriter;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.PriorityQueue;
 
-public class DB {
+import static constants.Constants.*;
+
+public class DB extends Main {
+
+    private static PriorityQueue<String> employPQ;
+
+    private static String[] searchKey = {"name", "namef", "namel", "cl", "phoneNum", "phoneNumf", "phoneNuml", "birthday", "birthdayy", "birthdaym", "birthdayd", "certi"};
+    private static String[] makeSubKey = {"name", "phoneNum", "birthday"};
+    private static String[] nameSubKey = {"namef", "namel"};
+    private static String[] phoneSubKey = {"phoneNumf", "phoneNuml"};
+    private static String[] birthSubKey = {"birthdayy", "birthdaym", "birthdayd"};
+
 
     private static void createSearchHM(EmployeeInfo newEmployeeInfo) {
-        String sEmployeeNum = newEmployeeInfo.getEmployeeNum();
 
-        String name = newEmployeeInfo.getName();
-        checkSearchHM("name_"+name, sEmployeeNum);
-
-        String namef = name.split(" ")[0];
-        checkSearchHM("namef_"+namef, sEmployeeNum);
-
-        String lname = name.split(" ")[1];
-        checkSearchHM("namel_"+lname, sEmployeeNum);
-
-        String cl = newEmployeeInfo.getCl();
-        checkSearchHM("cl"+cl, sEmployeeNum);
-
-        String phoneNum = newEmployeeInfo.getPhoneNum();
-        checkSearchHM("phoneNum_"+phoneNum, sEmployeeNum);
-
-        String fphone = phoneNum.split("-")[1];
-        checkSearchHM("phoneNumf_"+fphone, sEmployeeNum);
-
-        String lphone = phoneNum.split("-")[2];
-        checkSearchHM("phoneNuml_"+lphone, sEmployeeNum);
-
-        String birthday = newEmployeeInfo.getBirthday();
-        checkSearchHM("birthday_"+birthday, sEmployeeNum);
-
-        String ybirthday = birthday.substring(0, 4);
-        checkSearchHM("birthdayy_"+ybirthday, sEmployeeNum);
-
-        String mbirthday = "M" + birthday.substring(4, 6);
-        checkSearchHM("birthdaym_"+mbirthday, sEmployeeNum);
-
-        String dbirthday = "D" + birthday.substring(6, 8);
-        checkSearchHM("birthdayd_"+dbirthday, sEmployeeNum);
-
-        String certi = newEmployeeInfo.getCerti();
-        checkSearchHM("cl_"+certi, sEmployeeNum);
-
+        String sEmployeeNum = newEmployeeInfo.getObj("employeeNum");
+        for (String search : searchKey) {
+            checkSearchHM(search + newEmployeeInfo.getObj(search), sEmployeeNum);
+        }
+//        String sEmployeeNum = newEmployeeInfo.getEmployeeNum();
+//
+//
+//        String name = newEmployeeInfo.name;
+//        checkSearchHM("name" + name, sEmployeeNum);
+//
+//        String namef = name.split(" ")[0];
+//        checkSearchHM("namef" + namef, sEmployeeNum);
+//
+//        String lname = name.split(" ")[1];
+//        checkSearchHM("namel" + lname, sEmployeeNum);
+//
+//        String cl = newEmployeeInfo.cl;
+//        checkSearchHM("cl" + cl, sEmployeeNum);
+//
+//        String phoneNum = newEmployeeInfo.phoneNum;
+//        checkSearchHM("phoneNum" + phoneNum, sEmployeeNum);
+//
+//        String phoneNumf = phoneNum.split("-")[1];
+//        checkSearchHM("phoneNumf" + phoneNumf, sEmployeeNum);
+//
+//        String phoneNuml = phoneNum.split("-")[2];
+//        checkSearchHM("phoneNuml" + phoneNuml, sEmployeeNum);
+//
+//        String birthday = newEmployeeInfo.birthday;
+//        checkSearchHM("birthday" + birthday, sEmployeeNum);
+//
+//        String birthdayy = birthday.substring(0, 4);
+//        checkSearchHM("birthdayy" + birthdayy, sEmployeeNum);
+//
+//        String birthdaym = birthday.substring(4, 6);
+//        checkSearchHM("birthdaym" + birthdaym, sEmployeeNum);
+//
+//        String birthdayd = birthday.substring(6, 8);
+//        checkSearchHM("birthdayd" + birthdayd, sEmployeeNum);
+//
+//        String certi = newEmployeeInfo.getCerti();
+//        String certi = newEmployeeInfo.certi;
+//        checkSearchHM("certi" + certi, sEmployeeNum);
     }
 
     private static void checkSearchHM(String searchkey, String newEmployeeNum) {
-        if (Main.searchHM.get(searchkey) == null) {
-            // serachHM신규 생성.
+        if (searchHM.containsKey(searchkey)) {
+            searchHM.get(searchkey).add(newEmployeeNum);
+        } else {
             ArrayList<String> list = new ArrayList<String>();
             list.add(newEmployeeNum);
-            Main.searchHM.put(searchkey, list);
-        } else {
-            Main.searchHM.get(searchkey).add(newEmployeeNum);
+            searchHM.put(searchkey, list);
         }
     }
 
     public static void ADD(String[] tempsplit) {
+
+
         if (tempsplit.length < 10) System.out.println("ADD_FAIL :: Insert Null Data");
 
         EmployeeInfo newEmployeeInfo =
                 new EmployeeInfo(tempsplit[4], tempsplit[5], tempsplit[6], tempsplit[7], tempsplit[8], tempsplit[9]);
-        Main.employeeHM.put(tempsplit[4], newEmployeeInfo);
+        employeeHM.put(tempsplit[4], newEmployeeInfo);
 
         createSearchHM(newEmployeeInfo);
     }
 
-    public void DEL() {
+    public static String SCH(String[] tempsplit) {
+//        SCH,-p,-d, ,birthday,04
+        ArrayList<String> aResult = schResult(tempsplit);
+
+        //print option
+        return schPrint(aResult, (tempsplit[1].equals(" ") ? "NUMBER" : "STRING"));
     }
 
-    public void SCH() {
+    private static String schPrint(ArrayList<String> aResult, String sOption) {
+        String returnString = "";
+        if (aResult.size() == 0) return "NONE";
+        if (sOption.equals("STRING")) {
+
+            // 전체 LIST의 EMPLOYEE NO 기준으로 정렬하는 기능 추가 필요함.
+            int maxresult = 5;
+            if (aResult.size() < 5) maxresult = aResult.size();
+            for (int i = 0; i < maxresult; i++) {
+                returnString += employeeHM.get(aResult.get(i)).toString();
+            }
+        } else if (sOption.equals("NUMBER")) {
+            returnString = aResult.size() + "";
+        }
+
+        return returnString;
     }
 
-    public void MOD() {
+    //PriorityQueue를 이용해서
+    //1. 사번 순으로 정렬해서 저장.
+    //2. result에 동일 사번이 적재되지 않도록 개선.
+    private static ArrayList<String> schResult(String[] tempsplit) {
+
+//        employPQ = new PriorityQueue<>();
+        String searchKey = tempsplit[4];
+
+        if (!tempsplit[2].equals(" ")) {
+            searchKey += tempsplit[2].substring(1, 2);
+        }
+        String searchValue = searchKey + tempsplit[5];
+
+        ArrayList<String> resultList = new ArrayList<String>();
+
+        if (searchKey.equals("employeeNum")) {
+            if (employeeHM.containsKey(tempsplit[5]) && !employeeHM.get(tempsplit[5]).getRemoveFlag()) {
+                resultList.add(tempsplit[5]);
+            }
+        } else if (searchHM.containsKey(searchValue)) {
+            for (int i = 0; i < searchHM.get(searchValue).size(); i++) {
+                String sEmpNum = searchHM.get(searchValue).get(i);
+                if (employeeHM.get(sEmpNum).getRemoveFlag()) continue;
+                if (!employeeHM.get(sEmpNum).getObj(searchKey).equals(tempsplit[5])) continue;
+
+                resultList.add(sEmpNum);
+            }
+        }
+        return resultList;
     }
+
+    public static String DEL(String[] tempsplit) {
+        // 대상 조회
+        ArrayList<String> aResult = schResult(tempsplit);
+        // 프린트 옵션
+        String sSearchResult = schPrint(aResult, (tempsplit[1].equals(" ") ? "NUMBER" : "STRING"));
+
+        for (String empNum : aResult) {
+            employeeHM.get(empNum).setRemoveFlag();
+        }
+
+        return sSearchResult;
+    }
+
+
+    public static String MOD(String[] tempsplit) {
+        // 대상 조회
+        ArrayList<String> aResult = schResult(tempsplit);
+        // 프린트 옵션
+        String sSearchResult = schPrint(aResult, (tempsplit[1].equals(" ") ? "NUMBER" : "STRING"));
+
+
+        String modifyKey = tempsplit[6];
+        if (!tempsplit[2].equals(" ")) {
+            modifyKey += tempsplit[2].substring(1, 2);
+        }
+
+
+        //MOD,-p, , ,name,FB NTAWR,cl,CL3
+        for (String empNum : aResult) {
+            //원본 변경
+            employeeHM.get(empNum).setObj(tempsplit[6], tempsplit[7]);
+
+            checkSearchHM(modifyKey + employeeHM.get(empNum).getObj(modifyKey), empNum);
+            if (modifyKey.equals("name")) {
+                for (String subkey : nameSubKey) {
+                    checkSearchHM(subkey + employeeHM.get(empNum).getObj(subkey), empNum);
+                }
+            } else if (modifyKey.equals("phoneNum")) {
+                for (String subkey : phoneSubKey) {
+                    checkSearchHM(subkey + employeeHM.get(empNum).getObj(subkey), empNum);
+                }
+            } else if (modifyKey.equals("birthday")) {
+                for (String subkey : birthSubKey) {
+                    checkSearchHM(subkey + employeeHM.get(empNum).getObj(subkey), empNum);
+                }
+            }
+        }
+
+        return sSearchResult;
+    }
+
+
 }
