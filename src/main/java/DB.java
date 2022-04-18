6717e9d89623b96d6002bf84d283d61ce575c8b5
JaeHee <leejeahue@gmail.com>
2022-04-18 14:28:27 +0900

    priority queue 적용 (2)

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -17,61 +17,22 @@ public class DB extends Main {
     private static String[] phoneSubKey = {"phoneNumm", "phoneNuml"};
     private static String[] birthSubKey = {"birthdayy", "birthdaym", "birthdayd"};
 
-
     private static void createSearchHM(EmployeeInfo newEmployeeInfo) {
 
         String sEmployeeNum = newEmployeeInfo.getObj("employeeNum");
         for (String search : searchKey) {
             checkSearchHM(search + newEmployeeInfo.getObj(search), sEmployeeNum);
         }
-//        String sEmployeeNum = newEmployeeInfo.getEmployeeNum();
-//
-//
-//        String name = newEmployeeInfo.name;
-//        checkSearchHM("name" + name, sEmployeeNum);
-//
-//        String namef = name.split(" ")[0];
-//        checkSearchHM("namef" + namef, sEmployeeNum);
-//
-//        String lname = name.split(" ")[1];
-//        checkSearchHM("namel" + lname, sEmployeeNum);
-//
-//        String cl = newEmployeeInfo.cl;
-//        checkSearchHM("cl" + cl, sEmployeeNum);
-//
-//        String phoneNum = newEmployeeInfo.phoneNum;
-//        checkSearchHM("phoneNum" + phoneNum, sEmployeeNum);
-//
-//        String phoneNumf = phoneNum.split("-")[1];
-//        checkSearchHM("phoneNumf" + phoneNumf, sEmployeeNum);
-//
-//        String phoneNuml = phoneNum.split("-")[2];
-//        checkSearchHM("phoneNuml" + phoneNuml, sEmployeeNum);
-//
-//        String birthday = newEmployeeInfo.birthday;
-//        checkSearchHM("birthday" + birthday, sEmployeeNum);
-//
-//        String birthdayy = birthday.substring(0, 4);
-//        checkSearchHM("birthdayy" + birthdayy, sEmployeeNum);
-//
-//        String birthdaym = birthday.substring(4, 6);
-//        checkSearchHM("birthdaym" + birthdaym, sEmployeeNum);
-//
-//        String birthdayd = birthday.substring(6, 8);
-//        checkSearchHM("birthdayd" + birthdayd, sEmployeeNum);
-//
-//        String certi = newEmployeeInfo.getCerti();
-//        String certi = newEmployeeInfo.certi;
-//        checkSearchHM("certi" + certi, sEmployeeNum);
     }
+
 
     private static void checkSearchHM(String searchkey, String newEmployeeNum) {
         if (searchHM.containsKey(searchkey)) {
-            searchHM.get(searchkey).add(newEmployeeNum);
+            searchHM.get(searchkey).offer(new cmpString(newEmployeeNum));
         } else {
-            ArrayList<String> list = new ArrayList<String>();
-            list.add(newEmployeeNum);
-            searchHM.put(searchkey, list);
+            PriorityQueue<cmpString> pq = new PriorityQueue<>();
+            pq.offer(new cmpString(newEmployeeNum));
+            searchHM.put(searchkey, pq);
         }
     }
 
@@ -89,13 +50,13 @@ public class DB extends Main {
 
     public static String SCH(String[] tempsplit) {
 //        SCH,-p,-d, ,birthday,04
-        ArrayList<String> aResult = schResult(tempsplit);
+        PriorityQueue<cmpString> aResult = schResult(tempsplit);
 
         //print option
         return schPrint("SCH", aResult, (tempsplit[1].equals(" ") ? "NUMBER" : "STRING"));
     }
 
-    private static String schPrint(String mod, ArrayList<String> aResult, String sOption) {
+    private static String schPrint(String mod, PriorityQueue<cmpString> aResult, String sOption) {
         String returnString = "";
         if (aResult.size() == 0) return mod + "," + "NONE"+ "\n";
         if (sOption.equals("STRING")) {
@@ -104,7 +65,7 @@ public class DB extends Main {
             int maxresult = 5;
             if (aResult.size() < 5) maxresult = aResult.size();
             for (int i = 0; i < maxresult; i++) {
-                returnString += mod + "," + employeeHM.get(aResult.get(i)).toString() + "\n";
+                returnString += mod + "," + employeeHM.get(aResult.poll()).toString() + "\n";
             }
         } else if (sOption.equals("NUMBER")) {
             returnString = mod + "," + aResult.size() + "\n";
@@ -113,12 +74,10 @@ public class DB extends Main {
         return returnString;
     }
 
-    //PriorityQueue를 이용해서
-    //1. 사번 순으로 정렬해서 저장.
-    //2. result에 동일 사번이 적재되지 않도록 개선.
-    private static ArrayList<String> schResult(String[] tempsplit) {
+    private static PriorityQueue<cmpString> schResult(String[] tempsplit) {
 
 //        employPQ = new PriorityQueue<>();
+        PriorityQueue<cmpString> resultList=new PriorityQueue<>();
         String searchKey = tempsplit[4];
 
         if (!tempsplit[2].equals(" ")) {
@@ -126,41 +85,51 @@ public class DB extends Main {
         }
         String searchValue = searchKey + tempsplit[5];
 
-        ArrayList<String> resultList = new ArrayList<String>();
-
         if (searchKey.equals("employeeNum")) {
             if (employeeHM.containsKey(tempsplit[5]) && !employeeHM.get(tempsplit[5]).getRemoveFlag()) {
-                resultList.add(tempsplit[5]);
+                resultList.offer(new cmpString(tempsplit[5]));
             }
         } else if (searchHM.containsKey(searchValue)) {
-            for (int i = 0; i < searchHM.get(searchValue).size(); i++) {
-                String sEmpNum = searchHM.get(searchValue).get(i);
+            ArrayList<cmpString> cache=new ArrayList<>();
+            while(!searchHM.get(searchValue).isEmpty()){
+                String sEmpNum = searchHM.get(searchValue).poll().getEmployeeNum();
                 if (employeeHM.get(sEmpNum).getRemoveFlag()) continue;
                 if (!employeeHM.get(sEmpNum).getObj(searchKey).equals(tempsplit[5])) continue;
-
-                resultList.add(sEmpNum);
+                //refactoring 필요할듯합니다
+                if(cache.size()==0){
+                    cache.add(new cmpString(sEmpNum));
+                    resultList.offer(new cmpString(sEmpNum));
+                    continue;
+                }
+                else if(cache.size()!=0&&cache.get(cache.size()-1).getEmployeeNum().equals(sEmpNum))continue;
+                cache.add(new cmpString(sEmpNum));
+                resultList.offer(new cmpString(sEmpNum));
+            }
+            for(cmpString tmp:cache){
+                searchHM.get(searchValue).offer(tmp);
             }
         }
         return resultList;
     }
 
+
     public static String DEL(String[] tempsplit) {
         // 대상 조회
-        ArrayList<String> aResult = schResult(tempsplit);
+        PriorityQueue<cmpString> aResult = schResult(tempsplit);
         // 프린트 옵션
         String sSearchResult = schPrint("DEL", aResult, (tempsplit[1].equals(" ") ? "NUMBER" : "STRING"));
 
-        for (String empNum : aResult) {
-            employeeHM.get(empNum).setRemoveFlag();
+        for (cmpString empNum : aResult) {
+            employeeHM.get(empNum.getEmployeeNum()).setRemoveFlag();
         }
 
         return sSearchResult;
     }
 
 
-    public static String MOD(String[] tempsplit) {
+    public static String MODbyPQ(String[] tempsplit) {
         // 대상 조회
-        ArrayList<String> aResult = schResult(tempsplit);
+        PriorityQueue<cmpString> aResult = schResult(tempsplit);
         // 프린트 옵션
         String sSearchResult = schPrint("MOD", aResult, (tempsplit[1].equals(" ") ? "NUMBER" : "STRING"));
 
@@ -172,28 +141,27 @@ public class DB extends Main {
 
 
         //MOD,-p, , ,name,FB NTAWR,cl,CL3
-        for (String empNum : aResult) {
+        for (cmpString empNum : aResult) {
             //원본 변경
-            employeeHM.get(empNum).setObj(tempsplit[6], tempsplit[7]);
+            employeeHM.get(empNum.getEmployeeNum()).setObj(tempsplit[6], tempsplit[7]);
 
-            checkSearchHM(modifyKey + employeeHM.get(empNum).getObj(modifyKey), empNum);
+            checkSearchHM(modifyKey + employeeHM.get(empNum.getEmployeeNum()).getObj(modifyKey), empNum.getEmployeeNum());
             if (modifyKey.equals("name")) {
                 for (String subkey : nameSubKey) {
-                    checkSearchHM(subkey + employeeHM.get(empNum).getObj(subkey), empNum);
+                    checkSearchHM(subkey + employeeHM.get(empNum.getEmployeeNum()).getObj(subkey), empNum.getEmployeeNum());
                 }
             } else if (modifyKey.equals("phoneNum")) {
                 for (String subkey : phoneSubKey) {
-                    checkSearchHM(subkey + employeeHM.get(empNum).getObj(subkey), empNum);
+                    checkSearchHM(subkey + employeeHM.get(empNum.getEmployeeNum()).getObj(subkey), empNum.getEmployeeNum());
                 }
             } else if (modifyKey.equals("birthday")) {
                 for (String subkey : birthSubKey) {
-                    checkSearchHM(subkey + employeeHM.get(empNum).getObj(subkey), empNum);
+                    checkSearchHM(subkey + employeeHM.get(empNum.getEmployeeNum()).getObj(subkey), empNum.getEmployeeNum());
                 }
             }
         }
 
         return sSearchResult;
     }
-
 
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,47 @@
 import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.PriorityQueue;
 
 import static constants.Constants.*;
 
+class cmpString implements Comparable<cmpString>{
+    String employeeNum;
+
+    public cmpString(String employeeNum) {
+        this.employeeNum = employeeNum;
+    }
+
+    public String getEmployeeNum() {
+        return employeeNum;
+    }
+
+
+    @Override
+    public int compareTo(cmpString o) {
+        int originBirth=Integer.parseInt(transformbirth(this.employeeNum.substring(0,2)));
+        String originEmpno=this.employeeNum.substring(2,8);
+        int targetBirth=Integer.parseInt(transformbirth(o.employeeNum.substring(0,2)));
+        String targetEmpno=o.employeeNum.substring(2,8);
+        if(targetBirth!=originBirth)return (targetBirth-originBirth)>0?-1:1;
+        else{
+            return originEmpno.compareTo(targetEmpno);
+        }
+    }
+
+    private String transformbirth(String origin) {
+        if('0'<= origin.charAt(0)&& origin.charAt(0)<='2'){
+            return "1"+ origin;
+        }
+        return origin;
+    }
+}
+
 public class Main {
     protected static HashMap<String, EmployeeInfo> employeeHM;
-    protected static HashMap<String, ArrayList<String>> searchHM;
+    protected static HashMap<String, PriorityQueue<cmpString>> searchHM;
+
+
 
     private static void printOut(String printString) {
 
@@ -25,26 +60,38 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        BufferedReader br = new BufferedReader(new FileReader("./DIY/src/input.txt"));
+        //BufferedReader br = new BufferedReader(new FileReader("./DIY/src/input.txt"));
         employeeHM = new HashMap<>();
         searchHM = new HashMap<>();
+//        //pq order 확인
+//        System.out.println("012345".substring(0,2));
+//        PriorityQueue <cmpString> pq= new PriorityQueue<>();
+//        pq.offer(new cmpString("90123456"));
+//        pq.offer(new cmpString("72123456"));
+//        pq.offer(new cmpString("00123456"));
+//        pq.offer(new cmpString("00123456"));
+//        pq.offer(new cmpString("00323456"));
+//        pq.offer(new cmpString("00023456"));
+//        while (!pq.isEmpty()){
+//            System.out.println(pq.poll().getEmployeeNum());
+//        }
 
-        while (true) {
-            String temp = br.readLine();
-            if (temp == null) break;
-//            System.out.println(temp);
-            String[] tempsplit = temp.split(",");
-
-            if (tempsplit[0].equals(CMD_ADD)) {
-                DB.ADD(tempsplit);
-            } else if (tempsplit[0].equals(CMD_SCH)) {
-                printOut(DB.SCH(tempsplit));
-            } else if (tempsplit[0].equals(CMD_DEL)) {
-                printOut(DB.DEL(tempsplit));
-            } else if (tempsplit[0].equals(CMD_MOD)) {
-                printOut(DB.MOD(tempsplit));
-            }
-        }
+//        while (true) {
+//            String temp = br.readLine();
+//            if (temp == null) break;
+////            System.out.println(temp);
+//            String[] tempsplit = temp.split(",");
+//
+//            if (tempsplit[0].equals(CMD_ADD)) {
+//                DB.ADD(tempsplit);
+//            } else if (tempsplit[0].equals(CMD_SCH)) {
+//                printOut(DB.SCH(tempsplit));
+//            } else if (tempsplit[0].equals(CMD_DEL)) {
+//                printOut(DB.DEL(tempsplit));
+//            } else if (tempsplit[0].equals(CMD_MOD)) {
+//                printOut(DB.MOD(tempsplit));
+//            }
+//        }
 //
 //        System.out.println(employeeHM.size());
 //        System.out.println("employeeHM.get(\"12000001\").name :" + employeeHM.get("15123099").getName());
